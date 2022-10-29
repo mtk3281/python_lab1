3a881f6fbe94e8f969ffb2cbed899ca3099e3d92
Munthasar TK <96691215+mtk3281@users.noreply.github.com>
2022-10-29 22:54:01 +0530

    Delete 3swap.py

--- 3swap.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter the 1st no "))
-b=int(input("enter the 2nd no "))
-
-print(" 1st no before swapping = ",a)
-print(" 2nd no before swapping = ",b)
-a,b=b,a
-
-print(" 1st no after swapping = ",a)
-print(" 2nd no after swapping = ",b)
